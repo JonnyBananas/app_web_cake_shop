70415853906716dd3e7043bece25da76ff8ef0bc
JonnyBananas <Jonathanavarro16@gmail.com>
2016-11-23 16:53:34 -0700

    app_web_cake_shop
    actualizacion

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
+// se crea un servidor
 var app = express();
 
-app.set("view engine", "html");
+// se usa el body-Parser
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static("public"));
+// se declara el puerto
+const PORT = 8090;
 
-app.get("/", function(solicitud, respuesta));
-respuesta.render("index");
+// se agrega ruta fija /assets en directorio /public
+app.use('/', express.static(__dirname + '/public'));
+
+// se agrega un engine llamado 'html', el cual es 'atpl'
+app.engine('html', require('atpl').__express);
+
+// se especifica que el 'view engine' es 'html' (declarado previamente)
+app.set('view engine', 'html');
+
+app.set('devel', false);
+
+app.listen(PORT, function() {
+    console.log('El servidor esta listo [PORT:8090]');
 
 });
-
-app.listen(3000);
-console.log('Servidor listo! por el [puerto: 3000]');
